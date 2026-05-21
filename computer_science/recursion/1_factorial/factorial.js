c01a1554e2fb198368ee5691b1d250f8015064bc
fonsalir03 <194654788+fonsalir03@users.noreply.github.com>
2026-05-20 19:32:54 -0500

    solved factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function(int) {
+//if int is zero, return one
+if (int == 0){
+    return 1
+} else if (int < 0 || Number.isInteger(int) == false){
+    return undefined
+}
+return int * factorial(int - 1)
+//otherwise, return the function with int minus one
 };
 
 // Do not edit below this line
