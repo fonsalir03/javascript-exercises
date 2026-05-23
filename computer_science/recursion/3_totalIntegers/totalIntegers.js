dfcb5ab7f44fee4a102e8c2e9ea8300bba13e576
fonsalir03 <194654788+fonsalir03@users.noreply.github.com>
2026-05-22 19:07:28 -0500

    solved totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,26 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(arr) {
+    let total = 0
+
+    if (typeof arr != "object"){
+        return undefined
+    }
+    if (arr == null){
+        return total
+    }
+
+
+    const nums = Object.values(arr).filter((int)=> Number.isInteger(int))
+    if (nums.length >= 1){
+        total += nums.length
+    }
+
+    const nestedArray = Object.values(arr).filter((elem)=> typeof elem == "object")
+
+    
+    for (const item of nestedArray){
+        total += totalIntegers(item, total)
+    }
+    return total
 };
-  
 // Do not edit below this line
 module.exports = totalIntegers;
