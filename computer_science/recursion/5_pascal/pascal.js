c22b55825135d8b83aa7ee2a4c935e851d50a5c7
fonsalir03 <194654788+fonsalir03@users.noreply.github.com>
2026-05-25 10:07:11 -0500

    solved pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,26 @@
-const pascal = function() {
+const pascal = function(n) {
   
+    if (n == 1){
+        return [1]
+    }
+
+    if (n==2){
+        return [0+1,1+0]
+    }
+
+    let row = pascal(n-1)
+    
+    let newRow = [1]
+    for (i=0; i<row.length; i++){
+        newRow.push(row[i]+row[i+1])
+    }
+    newRow[newRow.length-1] = 1
+
+    return newRow
 };
-  
+
+res = pascal(5)
+console.log(res)  
+
 // Do not edit below this line
 module.exports = pascal;
